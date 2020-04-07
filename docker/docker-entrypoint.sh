63f3853f38b0e90d1cef2eb2b555801c54e1d791
printfuck <stuff.github@eris.cc>
2020-04-07 05:45:08 +0200

    no user due to permission fuckups

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ -f /tmp/.X10-lock ]; then rm /tmp/.X10-lock; fi
 Xvfb :10 -screen 0 1580x920x24 -ac &
-export DISPLAY=:10
+export DISPLAY=:10.0
 x11vnc -display :10 -rfbport 5900 -rfbportv6 -1 -no6 -noipv6 -httpportv6 -1 -forever -desktop StardewValley -cursor arrow -passwd $VNCPASS -shared & 
 sleep 5
 i3 &
