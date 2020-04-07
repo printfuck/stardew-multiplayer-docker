7bec5dc82510c7a8c3b07bd2938497f2fe653f82
printfuck <stuff.github@eris.cc>
2020-04-07 07:29:18 +0200

    fix for slow system; partly resolves #2

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,12 @@
 
 if [ -f /tmp/.X10-lock ]; then rm /tmp/.X10-lock; fi
 Xvfb :10 -screen 0 1580x920x24 -ac &
+
+while [ ! -z "`xdpyinfo -display :10 2>&1 | grep 'unable to open display'`" ]; do
+  echo Waiting for display;
+  sleep 5;
+done
+
 export DISPLAY=:10.0
 x11vnc -display :10 -rfbport 5900 -rfbportv6 -1 -no6 -noipv6 -httpportv6 -1 -forever -desktop StardewValley -cursor arrow -passwd $VNCPASS -shared & 
 sleep 5
