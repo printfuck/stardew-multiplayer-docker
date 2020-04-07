46c5b2f07990f5a6554121f418e3299e7b2bff0e
printfuck <stuff.github@eris.cc>
2020-04-07 02:25:05 +0200

    autoload mod added

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 Xvfb :10 -screen 0 1000x1000x24 -ac &
 export DISPLAY=:10.0
 x11vnc -display :10 -rfbport 5900 -rfbportv6 -1 -no6 -noipv6 -httpportv6 -1 -forever -desktop StardewValley -cursor arrow -shared -passwd 23eris42 & 
-sleep 10
+sleep 5
 i3 &
 
 TERM=xterm
