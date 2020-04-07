a373d461af959407499f6bfb70d4c3a44bf3a63a
printfuck <stuff.github@eris.cc>
2020-04-07 05:20:53 +0200

    user folder bugfixes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 if [ -f /tmp/.X10-lock ]; then rm /tmp/.X10-lock; fi
 Xvfb :10 -screen 0 1580x920x24 -ac &
-export DISPLAY=:10.0
-x11vnc -display :10 -rfbport 5900 -rfbportv6 -1 -no6 -noipv6 -httpportv6 -1 -forever -desktop StardewValley -cursor arrow -shared -passwd 23eris42 & 
+export DISPLAY=:10
+x11vnc -display :10 -rfbport 5900 -rfbportv6 -1 -no6 -noipv6 -httpportv6 -1 -forever -desktop StardewValley -cursor arrow -passwd $VNCPASS -shared & 
 sleep 5
 i3 &
 export XAUTHORITY=~/.Xauthority
