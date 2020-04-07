cf8015b63007c4c3a1ddb4ca52b2684931f7d7e9
printfuck <stuff.github@eris.cc>
2020-04-07 04:40:06 +0200

    now running as user

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+if [ -f /tmp/.X10-lock ]; then rm /tmp/.X10-lock; fi
 Xvfb :10 -screen 0 1580x920x24 -ac &
 export DISPLAY=:10.0
 x11vnc -display :10 -rfbport 5900 -rfbportv6 -1 -no6 -noipv6 -httpportv6 -1 -forever -desktop StardewValley -cursor arrow -shared -passwd 23eris42 & 
 sleep 5
 i3 &
-chmod +x /data/Stardew/Stardew\ Valley/StardewValley
-
+export XAUTHORITY=~/.Xauthority
 TERM=xterm
 /data/Stardew/Stardew\ Valley/StardewValley
