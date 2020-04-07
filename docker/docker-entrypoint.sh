ce24cf04222d1135d5861cd3ae42eecb9f26fc71
printfuck <stuff.github@eris.cc>
2020-04-07 03:52:52 +0200

    redoing configuration

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-Xvfb :10 -screen 0 1000x1000x24 -ac &
+Xvfb :10 -screen 0 1580x920x24 -ac &
 export DISPLAY=:10.0
 x11vnc -display :10 -rfbport 5900 -rfbportv6 -1 -no6 -noipv6 -httpportv6 -1 -forever -desktop StardewValley -cursor arrow -shared -passwd 23eris42 & 
 sleep 5
 i3 &
+chmod +x /data/Stardew/Stardew\ Valley/StardewValley
 
 TERM=xterm
 /data/Stardew/Stardew\ Valley/StardewValley
