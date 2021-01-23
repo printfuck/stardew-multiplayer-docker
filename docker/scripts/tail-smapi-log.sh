da8f413197a1968b4fbbe506375b9ffa6c3995fd
root <root@offsite>
2021-01-24 00:04:36 +0100

    fix

--- docker/scripts/tail-smapi-log.sh
+++ docker/scripts/tail-smapi-log.sh
@@ -3,7 +3,7 @@
 echo "-- SMAPI Log: Starting"
 
 # Wait for SMAPI log and tail infinitely
-while [ ! -f "/root/.config/StardewValley/ErrorLogs/SMAPI-latest.txt" ]; do
+while [ ! -f "/config/xdg/config/StardewValley/ErrorLogs/SMAPI-latest.txt" ]; do
   echo "-- SMAPI Log: Waiting for log to appear";
   sleep 5;
 done
